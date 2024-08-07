0a83aa0f8ebb24caace86f828b7dbcfd86580521
Isabella Nobilé <imnobile95@gmail.com>
2024-08-07 15:12:46 -0500

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC! I spend too much time here. I use Python for server-side scripts, and JavaScript when I want to make my life more difficult.
+    Here is my <a href="https://github.com/izzynobile" target="_blank">Github</a>!`,
+  "cs-degree": `This is my <a href="https://www.linkedin.com/in/imnobile/overlay/1635462738450/single-media-viewer/?profileId=ACoAABiEL-kB3MkTDT-Ajs-GgvMKF64iPUSFt9I" target="_blank">UX Design Certificate from General Assembly.</a> I hung it on the wall because this place needs decorating and my unicorn posters are still in the mail.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch <a href="https://www.youtube.com/watch?v=gtPxuC60GU4" target="_blank">DropoutTV</a>. There's nothing I love more than unhinged comedy.`,
+  tv: `That's my TV. I've been watching the Broski Report nonstop. If you also want to experience the cultural phenomenon that is Brittany Broski look no further :
+   <a href="https://youtu.be/wWBuRGq-3XA?si=Zco6lys3cIbzBXqV" target="_blank">TheBroskiReport</a>`,
+  bed: `This where I sleep and take my celebratory debug naps.`,
+  resume: `This is my resume. <a href="https://github.com/izzynobile/Resume/blob/main/INobile_August_Resume_2024.pdf" target="_blank">Check it out?</a>
+  Contact me at imnobile95@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of JSLegendDev's tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `Currently reading 'The Power of Now.' You might catch me at one of Austin's Silent Book Club meetups. Check them out <a href="https://silentbook.club/blogs/events/silent-book-club-austin" target="_blank">here!</a>`,
+  exit: `If you want to exit Izzy's portfolio, just close the tab.`,
+};
